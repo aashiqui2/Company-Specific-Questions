e25b24a8a7ef09abc71d6290f264c475ae03a5ab
Aashiqui <ashikmail2747@gmail.com>
2026-02-03 00:10:14 +0530

    New Problem Added

---
+++ Demo11.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Demo11 {
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 2, 2, 2, 5, 5, 108, 3, 1 };
+        replaceDuplicates(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /*public static void replaceDuplicates(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            boolean first = true;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] == arr[i]) {
+                    first = false;
+                    break;
+                }
+            }
+
+            if (!first) {
+                int candidate = 1;
+                while (contains(arr, candidate)) {
+                    candidate++;
+                }
+                arr[i] = candidate;
+            }
+        }
+    }
+
+    public static boolean contains(int[] arr, int val) {
+        for (int x : arr) {
+            if (x == val)
+                return true;
+        }
+        return false;
+    }*/
+
+    public static void replaceDuplicates(int[] arr) {
+        Set<Integer> used = new HashSet<>();
+        for (int x : arr) {
+            used.add(x);
+        }
+        int nextMin = 1;
+        for (int i = 0; i < arr.length; i++) {
+            boolean first = true;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] == arr[i]) {
+                    first = false;
+                    break;
+                }
+            }
+
+            if (!first) {
+                while (used.contains(nextMin)) {
+                    nextMin++;
+                }
+
+                used.remove(arr[i]); // remove old duplicate
+                arr[i] = nextMin;
+                used.add(nextMin);
+                nextMin++;
+            }
+        }
+    }
+}
